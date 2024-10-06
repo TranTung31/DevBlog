ba0314bf1f4636f1a672ee528f735ad6cbe5a671
Your Name <you@example.com>
2024-10-06 20:19:52 +0700

    feat: migrate database

---
+++ src/DevBlog.Infrastructure/DevBlogDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DevBlog.Infrastructure
+{
+    public class DevBlogDbContextFactory : IDesignTimeDbContextFactory<DevBlogDbContext>
+    {
+        public DevBlogDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DevBlogDbContext>();
+
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new DevBlogDbContext(builder.Options);
+        }
+    }
+}
