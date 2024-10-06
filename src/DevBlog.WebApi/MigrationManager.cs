904190438d1d86d45ae116c08ec1bb7e15bbf8a4
Your Name <you@example.com>
2024-10-06 21:09:49 +0700

    feat: data seeding

---
+++ src/DevBlog.WebApi/MigrationManager.cs
@@ -0,0 +1,22 @@
+﻿using DevBlog.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBlog.WebApi
+{
+    public static class MigrationManager
+    {
+        public static WebApplication MigrateDatabase(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                using (var context = scope.ServiceProvider.GetRequiredService<DevBlogDbContext>())
+                {
+                    context.Database.Migrate();
+                    new DataSeeder().SeedAsync(context).Wait();
+                }
+            }
+
+            return app;
+        }
+    }
+}
